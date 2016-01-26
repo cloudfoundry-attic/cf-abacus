63cccfc010b742b56d5945093f761f1423dcd2ec
Dan Parsons <dparsons@nyip.net>
2016-01-26 13:24:11 -0800

    added -t parameter to specify reporting timestamp

--- demo/client/src/test/report.js
+++ demo/client/src/test/report.js
@@ -13,6 +13,9 @@ commander
   .option('-o, --org <guid>',
     'guid of cf organization to report on [us-south:a3d7fe4d-3cb1-4cc3-a831-ffe98e20cf27]',
     'us-south:a3d7fe4d-3cb1-4cc3-a831-ffe98e20cf27')
+  .option('-t, --time <timestamp> [1420502400000]',
+    'timestamp to report on',
+    '1420502400000')
   .parse(process.argv);
 
 // Reporting service URL
@@ -23,7 +26,7 @@ const reporting = /:/.test(commander.reporting) ? commander.reporting :
 request.get(reporting +
   '/v1/metering/organizations/:organization_id/aggregated/usage/:time', {
     organization_id: commander.org,
-    time: 1420502400000
+    time: commander.time
   }, (err, val) => {
     if(err)
       console.log('Response', err);
