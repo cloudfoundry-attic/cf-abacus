f57598b8b63514d79eb8996aca330364128312f9
Benjamin Cheng <bscheng@us.ibm.com>
2016-02-12 15:57:38 -0800

    Adjust accumulator itests to use abacus-timewindow

--- test/aggregation/accumulator/src/test/test.js
+++ test/aggregation/accumulator/src/test/test.js
@@ -14,6 +14,7 @@ const seqid = require('abacus-seqid');
 const router = require('abacus-router');
 const express = require('abacus-express');
 const clone = require('abacus-clone');
+const timewindow = require('abacus-timewindow');
 
 const BigNumber = require('bignumber.js');
 BigNumber.config({ ERRORS: false });
@@ -79,54 +80,29 @@ const pruneWindows = (v, k) => {
   return v;
 };
 
-// Converts a millisecond number to a format a number that is YYYYMMDDHHmmSS
-const dateUTCNumbify = (t) => {
-  const d = new Date(t);
-  return d.getUTCFullYear() * 10000000000 + d.getUTCMonth() * 100000000
-    + d.getUTCDate() * 1000000 + d.getUTCHours() * 10000 + d.getUTCMinutes()
-    * 100 + d.getUTCSeconds();
-};
-
-// Converts a number output in dateUTCNumbify back to a Date object
-const revertUTCNumber = (n) => {
-  const numstring = n.toString();
-  const d = new Date(Date.UTC(
-    numstring.substring(0, 4),
-    numstring.substring(4, 6),
-    numstring.substring(6, 8),
-    numstring.substring(8, 10),
-    numstring.substring(10, 12),
-    numstring.substring(12)
-  ));
-  return d;
-};
-
 // Calculates the accumulated quantity given an end time, u, window size,
 // and multiplier factor of the usage
 const calculateQuantityByWindow = (e, u, w, m, f) => {
-  const time = e + u;
-  const timeNum = dateUTCNumbify(time);
-  const windowTimeNum = Math.floor(timeNum / w) * w;
-
+  const time = new Date(e + u);
   // Get the millisecond equivalent of the very start of the given window
-  const windowTime = revertUTCNumber(windowTimeNum).getTime();
-  return f(m, Math.min(time - windowTime, u));
+  return f(m, Math.min(time.getTime() -
+    timewindow.zeroLowerTimeDimensions(time, w).getTime(), u));
 };
 
 // Builds the quantity array in the accumulated usage
 const buildQuantityWindows = (e, u, m, f, price) => {
   // Scaling factor for a time window
   // [Second, Minute, Hour, Day, Month]
-  const timescale = [1, 100, 10000, 1000000, 100000000];
-  const windows = map(timescale, (ts) => {
+  const dimensions = ['s', 'm', 'h', 'D', 'M'];
+  const windows = map(dimensions, (d) => {
     // If this is the first usage, only return current
     if(u === 0)
       return [{ quantity: { current: f(m, u + 1) } }];
     // Return a properly accumulated current & previous
     return [{
       quantity: {
-        previous: calculateQuantityByWindow(e, u, ts, m, f),
-        current: calculateQuantityByWindow(e, u + 1, ts, m, f) } }];
+        previous: calculateQuantityByWindow(e, u, d, m, f),
+        current: calculateQuantityByWindow(e, u + 1, d, m, f) } }];
   });
 
   return map(windows, (w) => map(w, (q) => extend(q, {
