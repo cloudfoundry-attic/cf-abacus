7fb697bd1d0a6ddc7d4f1a1bbfb7f5773fb984d6
Kevin R. Yudhiswara <kryudhis@us.ibm.com>
2016-08-23 22:10:33 -0700

    Modification to the itest and aggregator test

---
+++ test/cf/bridge/src/test/slack-window-test.js
@@ -0,0 +1,513 @@
+'use strict';
+
+const commander = require('commander');
+const cp = require('child_process');
+const jwt = require('jsonwebtoken');
+const util = require('util');
+
+const _ = require('underscore');
+const clone = _.clone;
+
+const dbclient = require('abacus-dbclient');
+const express = require('abacus-express');
+const request = require('abacus-request');
+const router = require('abacus-router');
+
+// Setup the debug log
+const debug =
+  require('abacus-debug')('abacus-cf-bridge-itest');
+const responseDebug =
+  require('abacus-debug')('abacus-cf-bridge-itest-response');
+const resultDebug =
+  require('abacus-debug')('abacus-cf-bridge-itest-result');
+const oAuthDebug =
+  require('abacus-debug')('abacus-cf-bridge-itest-oauth');
+
+// Module directory
+const moduleDir = (module) => {
+  const path = require.resolve(module);
+  return path.substr(0, path.indexOf(module + '/') + module.length);
+};
+
+const timeWindows = {
+  'second' : 0,
+  'minute' : 1,
+  'hour'   : 2,
+  'day'    : 3,
+  'month'  : 4
+};
+
+// Checks if the difference between start and end time fall within a window
+const isWithinWindow = (start, end, timeWindow) => {
+  // [Second, Minute, Hour, Day, Month]
+  const timescale = [1, 100, 10000, 1000000, 100000000];
+  // Converts a millisecond number to a format a number that is YYYYMMDDHHmmSS
+  const dateUTCNumbify = (t) => {
+    const d = new Date(t);
+    return d.getUTCFullYear() * 10000000000 + d.getUTCMonth() * timescale[4]
+      + d.getUTCDate() * timescale[3] + d.getUTCHours() * timescale[2]
+      + d.getUTCMinutes() * timescale[1] + d.getUTCSeconds();
+  };
+
+  return Math.floor(dateUTCNumbify(end) / timescale[timeWindow]) -
+    Math.floor(dateUTCNumbify(start) / timescale[timeWindow]) === 0;
+};
+
+// Parse command line options
+const argv = clone(process.argv);
+argv.splice(1, 1, 'usage-collector-itest');
+commander
+  .option('-t, --start-timeout <n>',
+    'external processes start timeout in milliseconds', parseInt)
+  .option('-x, --total-timeout <n>',
+    'test timeout in milliseconds', parseInt)
+  .allowUnknownOption(true)
+  .parse(argv);
+
+// External Abacus processes start timeout
+const startTimeout = commander.startTimeout || 100000;
+
+// This test timeout
+const totalTimeout = commander.totalTimeout || 200000;
+
+// Token setup
+const tokenSecret = 'secret';
+const tokenAlgorithm = 'HS256';
+const resourceToken = {
+  header: {
+    alg: tokenAlgorithm
+  },
+  payload: {
+    jti: '254abca5-1c25-40c5-99d7-2cc641791517',
+    sub: 'abacus-cf-bridge',
+    authorities: [
+      'abacus.usage.linux-container.write',
+      'abacus.usage.linux-container.read'
+    ],
+    scope: [
+      'abacus.usage.linux-container.read',
+      'abacus.usage.linux-container.write'
+    ],
+    client_id: 'abacus-cf-bridge',
+    cid: 'abacus-cf-bridge',
+    azp: 'abacus-cf-bridge',
+    grant_type: 'client_credentials',
+    rev_sig: '2cf89595',
+    iat: 1456147679,
+    exp: 1456190879,
+    iss: 'https://localhost:1234/oauth/token',
+    zid: 'uaa',
+    aud: [
+      'abacus-cf-bridge',
+      'abacus.usage.linux-container'
+    ]
+  },
+  signature: 'irxoV230hkDJenXoTSHQFfqzoUl353lS2URo1fJm21Y'
+};
+const systemToken = {
+  header: {
+    alg: tokenAlgorithm
+  },
+  payload: {
+    jti: '254abca5-1c25-40c5-99d7-2cc641791517',
+    sub: 'abacus-cf-bridge',
+    authorities: [
+      'abacus.usage.write',
+      'abacus.usage.read'
+    ],
+    scope: [
+      'abacus.usage.write',
+      'abacus.usage.read'
+    ],
+    client_id: 'abacus-cf-bridge',
+    cid: 'abacus-cf-bridge',
+    azp: 'abacus-cf-bridge',
+    grant_type: 'client_credentials',
+    rev_sig: '2cf89595',
+    iat: 1456147679,
+    exp: 1456190879,
+    iss: 'https://localhost:1234/oauth/token',
+    zid: 'uaa',
+    aud: [
+      'abacus-cf-bridge',
+      'abacus.usage'
+    ]
+  },
+  signature: 'OVNTKTvu-yHI6QXmYxtPeJZofNddX36Mx1q4PDWuYQE'
+};
+const signedResourceToken = jwt.sign(resourceToken.payload, tokenSecret, {
+  expiresIn: 43200
+});
+const signedSystemToken = jwt.sign(systemToken.payload, tokenSecret, {
+  expiresIn: 43200
+});
+
+const sixDaysInMilliseconds = 6 * 24 * 60 * 60 * 1000;
+const twentySecondsInMilliseconds = 20 * 1000;
+
+const test = (secured) => {
+  const submittime = Date.now();
+
+  let server;
+  let serverPort;
+  let appUsageEvents;
+  let expectedConsuming;
+
+  beforeEach((done) => {
+    const start = (module) => {
+      debug('Starting %s in directory %s', module, moduleDir(module));
+      const c = cp.spawn('npm', ['run', 'start'], {
+        cwd: moduleDir(module),
+        env: clone(process.env)
+      });
+
+      // Add listeners to stdout, stderr and exit message and forward the
+      // messages to debug logs
+      c.stdout.on('data', (data) => process.stdout.write(data));
+      c.stderr.on('data', (data) => process.stderr.write(data));
+      c.on('exit', (code) => debug('Module %s started with code %d',
+        module, code));
+    };
+
+    const app = express();
+    const routes = router();
+    routes.get('/v2/app_usage_events', (request, response) => {
+      if (request.url.indexOf('after_guid') !== -1) {
+        debug('Returning empty list of usage events');
+        response.status(200).send({
+          total_results: 0,
+          total_pages: 0,
+          prev_url: null,
+          next_url: null,
+          resources: []
+        });
+        return;
+      }
+
+      response.status(200).send({
+        total_results: appUsageEvents.length,
+        total_pages: 1,
+        prev_url: null,
+        next_url: null,
+        resources: appUsageEvents
+      });
+    });
+    routes.get('/v2/info', (request, response) => {
+      oAuthDebug('Requested API info');
+      response.status(200).send({
+        token_endpoint: 'http://localhost:' + serverPort
+      });
+    });
+    routes.get('/oauth/token', (request, response) => {
+      oAuthDebug('Requested oAuth token with %j', request.query);
+      const scope = request.query.scope;
+      const containerToken = scope && scope.indexOf('container') > 0;
+      response.status(200).send({
+        token_type: 'bearer',
+        access_token: containerToken ? signedResourceToken : signedSystemToken,
+        expires_in: 100000,
+        scope: scope ? scope.split(' ') : '',
+        authorities: scope ? scope.split(' ') : '',
+        jti: '254abca5-1c25-40c5-99d7-2cc641791517'
+      });
+    });
+    app.use(routes);
+    app.use(router.batch(routes));
+    server = app.listen(0);
+    serverPort = server.address().port;
+    debug('Test resources server listening on port %d', serverPort);
+
+    // Enable/disable the oAuth token authorization
+    process.env.SECURED = secured ? 'true' : 'false';
+    debug('Set SECURED = %s', process.env.SECURED);
+
+    // Set environment variables
+    process.env.API = 'http://localhost:' + serverPort;
+    process.env.AUTH_SERVER = 'http://localhost:' + serverPort;
+    process.env.CF_CLIENT_ID = 'abacus-cf-bridge';
+    process.env.CF_CLIENT_SECRET = 'secret';
+    process.env.CLIENT_ID = 'abacus-linux-container';
+    process.env.CLIENT_SECRET = 'secret';
+    process.env.JWTKEY = tokenSecret;
+    process.env.JWTALGO = tokenAlgorithm;
+
+    // Set slack window to 5 days
+    process.env.SLACK = '5D';
+
+    // Disable wait for correct app-event ordering
+    process.env.GUID_MIN_AGE = twentySecondsInMilliseconds;
+
+    // Start all Abacus services
+    const services = () => {
+      start('abacus-eureka-plugin');
+      start('abacus-provisioning-plugin');
+      start('abacus-account-plugin');
+      start('abacus-usage-collector');
+      start('abacus-usage-meter');
+      start('abacus-usage-accumulator');
+      start('abacus-usage-aggregator');
+      start('abacus-usage-reporting');
+      start('abacus-cf-bridge');
+
+      done();
+    };
+
+    // Start local database server
+    if (!process.env.DB) {
+      start('abacus-pouchserver');
+      services();
+    }
+    else
+      // Delete test dbs on the configured db server
+      dbclient.drop(process.env.DB, /^abacus-/, () => {
+        services();
+      });
+  });
+
+  afterEach((done) => {
+    let counter = 10;
+    const finishCb = (module, code) => {
+      counter--;
+      debug('Module %s exited with code %d. Left %d modules',
+        module, code, counter);
+      if (counter === 0) {
+        debug('All modules stopped. Exiting test');
+        done();
+      }
+    };
+
+    const stop = (module, cb) => {
+      debug('Stopping %s in directory %s', module, moduleDir(module));
+      const c = cp.spawn('npm', ['run', 'stop'],
+        { cwd: moduleDir(module), env: clone(process.env) });
+
+      // Add listeners to stdout, stderr and exit message and forward the
+      // messages to debug logs
+      c.stdout.on('data', (data) => process.stdout.write(data));
+      c.stderr.on('data', (data) => process.stderr.write(data));
+      c.on('exit', (code) => cb(module, code));
+    };
+
+    stop('abacus-cf-bridge', finishCb);
+    stop('abacus-usage-reporting', finishCb);
+    stop('abacus-usage-aggregator', finishCb);
+    stop('abacus-usage-accumulator', finishCb);
+    stop('abacus-usage-meter', finishCb);
+    stop('abacus-usage-collector', finishCb);
+    stop('abacus-account-plugin', finishCb);
+    stop('abacus-provisioning-plugin', finishCb);
+    stop('abacus-eureka-plugin', finishCb);
+    stop('abacus-pouchserver', finishCb);
+
+    server.close();
+
+    delete process.env.SECURED;
+    delete process.env.API;
+    delete process.env.AUTH_SERVER;
+    delete process.env.CF_CLIENT_ID;
+    delete process.env.CF_CLIENT_SECRET;
+    delete process.env.CLIENT_ID;
+    delete process.env.CLIENT_SECRET;
+    delete process.env.JWTKEY;
+    delete process.env.JWTALGO;
+    delete process.env.SLACK;
+    delete process.env.GUID_MIN_AGE;
+  });
+
+<<<<<<< HEAD:test/cf/bridge/src/test/slack-window-test.js
+  const checkAllTimeWindows = (usage, reporttime, level) => {
+    for (const windowType in timeWindows)
+      if(isWithinWindow(submittime, reporttime, timeWindows[windowType])) {
+        const windowUsage = usage.windows[timeWindows[windowType]];
+        if(level !== 'resource')
+          expect(windowUsage[0].quantity.consuming).to.equal(expectedConsuming);
+=======
+  const checkAllTimeWindows = (usage, reporttime) => {
+    for (const windowType in timeWindows)
+      if(isWithinWindow(submittime, reporttime, timeWindows[windowType])) {
+        const windowUsage = usage.windows[timeWindows[windowType]];
+        expect(windowUsage[0].quantity.consuming).to.equal(expectedConsuming);
+>>>>>>> upstream/master:test/cf/bridge/src/test/slack-window-test.js
+        expect(windowUsage[0].charge).to.be.above(0);
+      }
+  };
+
+  const checkReport = (cb) => {
+    request.get('http://localhost:9088/v1/metering/organizations' +
+      '/:organization_id/aggregated/usage', {
+        organization_id: 'e8139b76-e829-4af3-b332-87316b1c0a6c',
+        headers: {
+          authorization: 'bearer ' + signedResourceToken
+        }
+      },
+      (error, response) => {
+        try {
+          expect(error).to.equal(undefined);
+
+          expect(response.body).to.contain.all.keys('resources', 'spaces');
+          const resources = response.body.resources;
+          expect(resources.length).to.equal(1);
+          expect(response.body.spaces.length).to.equal(1);
+          const reporttime = Date.now();
+
+          expect(resources[0]).to.contain.all.keys(
+            'plans', 'aggregated_usage');
+
+          const planUsage = resources[0].plans[0].aggregated_usage[0];
+          checkAllTimeWindows(planUsage, reporttime);
+
+          const aggregatedUsage = resources[0].aggregated_usage[0];
+<<<<<<< HEAD:test/cf/bridge/src/test/slack-window-test.js
+
+          checkAllTimeWindows(aggregatedUsage, reporttime, 'resource');
+
+=======
+          checkAllTimeWindows(aggregatedUsage, reporttime);
+
+>>>>>>> upstream/master:test/cf/bridge/src/test/slack-window-test.js
+          resultDebug('All usage report checks are successful for: %s',
+            JSON.stringify(response.body, null, 2));
+
+          cb();
+        }
+        catch (e) {
+          const message = util.format('Check failed with %s.\n' +
+            'Usage report:\n', e.stack,
+            response ? JSON.stringify(response.body, null, 2) : undefined);
+          responseDebug(message);
+          cb(new Error(message), e);
+        }
+      });
+  };
+
+  const poll = (fn, done, timeout = 1000, interval = 100) => {
+    const startTimestamp = Date.now();
+
+    const doneCallback = (err) => {
+      if (!err) {
+        debug('Expectation in %s met', fn.name);
+        setImmediate(() => done());
+        return;
+      }
+
+      if (Date.now() - startTimestamp > timeout) {
+        debug('Expectation not met for %d ms. Error: %o', timeout, err);
+        setImmediate(() => done(new Error(err)));
+      }
+      else
+        setTimeout(() => {
+          debug('Calling %s after >= %d ms...', fn.name, interval);
+          fn(doneCallback);
+        }, interval);
+    };
+
+    debug('Calling %s for the first time...', fn.name);
+    fn(doneCallback);
+  };
+
+  const waitForStartAndPoll = (component, port, done) => {
+    // Wait for bridge to start
+    let startWaitTime = Date.now();
+    request.waitFor('http://localhost::p/v1/cf/:component',
+      { component: component, p: port },
+      startTimeout, (err, uri, opts) => {
+        // Failed to ping component before timing out
+        if (err) throw err;
+
+        // Check report
+        request.get(uri, {
+          headers: {
+            authorization: secured ? 'bearer ' + signedSystemToken : ''
+          }
+        }, (err, response) => {
+          expect(err).to.equal(undefined);
+          expect(response.statusCode).to.equal(200);
+
+          poll(checkReport, (error) => {
+            done(error);
+          }, totalTimeout - (Date.now() - startWaitTime), 1000);
+        });
+      }
+    );
+  };
+
+  context('when submitting out of slack usage', () => {
+    beforeEach(() => {
+      appUsageEvents = [
+        // Usage that will be rejected because of the slack window
+        {
+          metadata: {
+            guid: '904419c3',
+            url: '/v2/app_usage_events/904419c3',
+            created_at:
+              new Date(submittime - sixDaysInMilliseconds).toISOString()
+          },
+          entity: {
+            state: 'STARTED',
+            previous_state: 'STOPPED',
+            memory_in_mb_per_instance: 256,
+            previous_memory_in_mb_per_instance: 0,
+            instance_count: 1,
+            previous_instance_count: 0,
+            app_guid: '35c4ff2f',
+            app_name: 'app',
+            space_guid: 'a7e44fcd-25bf-4023-8a87-03fba4882995',
+            space_name: 'abacus',
+            org_guid: 'e8139b76-e829-4af3-b332-87316b1c0a6c',
+            buildpack_guid: null,
+            buildpack_name: null,
+            package_state: 'PENDING',
+            previous_package_state: 'PENDING',
+            parent_app_guid: null,
+            parent_app_name: null,
+            process_type: 'web'
+          }
+        },
+        // Usage that has to be processed by the pipeline
+        {
+          metadata: {
+            guid: '904419c4',
+            url: '/v2/app_usage_events/904419c4',
+            created_at:
+              new Date(submittime - twentySecondsInMilliseconds).toISOString()
+          },
+          entity: {
+            state: 'STARTED',
+            previous_state: 'STOPPED',
+            memory_in_mb_per_instance: 512,
+            previous_memory_in_mb_per_instance: 0,
+            instance_count: 1,
+            previous_instance_count: 0,
+            app_guid: '35c4ff2f',
+            app_name: 'app',
+            space_guid: 'a7e44fcd-25bf-4023-8a87-03fba4882995',
+            space_name: 'abacus',
+            org_guid: 'e8139b76-e829-4af3-b332-87316b1c0a6c',
+            buildpack_guid: null,
+            buildpack_name: null,
+            package_state: 'PENDING',
+            previous_package_state: 'PENDING',
+            parent_app_guid: null,
+            parent_app_name: null,
+            process_type: 'web'
+          }
+        }
+      ];
+
+      // first start is ignored - out of slack window
+      // second start: 0.5 GB
+      expectedConsuming = 0.5;
+    });
+
+    it('usage is rejected', function(done) {
+      this.timeout(totalTimeout + 2000);
+
+      waitForStartAndPoll('bridge', 9500, done);
+    });
+  });
+
+};
+
+describe('abacus-cf-bridge slack-window-test without oAuth', () => test(false));
+
+describe('abacus-cf-bridge slack-window-test with oAuth', () => test(true));
