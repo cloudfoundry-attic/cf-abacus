9c1e93c184e476d58272eabc44d72666664b5dd8
Adriana Stefanova <adriana.stefanova@sap.com>
2016-10-31 10:57:13 +0200

    Add cf-monitor pipeline
    Signed-off-by: Stoyan Gradev <stoyan.gradev@sap.com>

---
+++ etc/concourse/scripts/riemann-metric-failure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$SEND_TO_RIEMANN" = true ]
+then
+  testresult=0 #test failed
+  testmetric="$CF_DEPLOYMENT_NAME.abacus.uptime $testresult $(date +%s)"
+
+  echo "Reporting metric: $testmetric to $RIEMANN_ADDRESS"
+  timeout_in_sec=30
+  echo $testmetric | nc -q $timeout_in_sec $RIEMANN_ADDRESS $RIEMANN_PORT
+  exit 0
+fi
