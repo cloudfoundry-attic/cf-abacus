0c9bd7a8a8abc6ec0814c186657f46c14c3cecef
HristoDobchev <hristo.dobtchev@sap.com>
2017-06-15 10:12:03 +0300

    Modify cfpack to process shrinkwrap files. Add tests

--- tools/cfpack/src/index.js
+++ tools/cfpack/src/index.js
@@ -22,7 +22,7 @@ const additionalDir = 'additionally_packed';
 
 const makeSymlink = (additionalDirPath, path, cb) => {
   fs.unlink(path, (err) => {
-    if(err) noop();
+    if (err) noop();
     fs.symlink(additionalDirPath, path, cb);
   });
 };
@@ -31,17 +31,17 @@ const makeSymlink = (additionalDirPath, path, cb) => {
 const mkdirs = (root, additional, cb) => {
   // Create .cfpack directory
   fs.mkdir('.cfpack', (err) => {
-    if(err) noop();
+    if (err) noop();
     makeSymlink(path.join(root, 'lib'), '.cfpack/lib', () => {
-      if(err) noop();
+      if (err) noop();
       else if (isEmpty(additional)) cb();
       else {
         const additionalDirPath = path.join(root, additional);
         fs.access(additionalDirPath, (err) => {
-          if(err) cb();
+          if (err) cb();
           else
-          makeSymlink(additionalDirPath,
-            path.join('.cfpack', additionalDir), cb);
+            makeSymlink(additionalDirPath,
+              path.join('.cfpack', additionalDir), cb);
         });
       }
     });
@@ -58,6 +58,20 @@ const local = (root, additional, d) => {
   return !/file:/.test(d[1]) ? d : [d[0], dependencyPath];
 };
 
+// Fixes dependencies locations for npm-shrinkwrap.json
+const fixShwinkwrapDeps = (deps, loc) => {
+  map(pairs(deps), (dep) => {
+    const name = dep[0];
+    const val = dep[1];
+    const newPath = loc([name, val.resolved])[1];
+    deps[name].resolved = newPath;
+    if (/file:/.test(newPath))
+      deps[name].from = newPath.substr(5);
+    if (val.dependencies)
+      fixShwinkwrapDeps(val.dependencies, loc);
+  });
+};
+
 // Adjust local dependencies to our packed app structure and write new
 // package.json.
 const repackage = (root, additional, cb) => {
@@ -69,14 +83,24 @@ const repackage = (root, additional, cb) => {
     devDependencies: object(map(pairs(mod.devDependencies), loc))
   });
   fs.writeFile(path.join('.cfpack', 'package.json'),
-    JSON.stringify(rmod, undefined, 2), cb);
+    JSON.stringify(rmod, undefined, 2), (err) => {
+      const shWrpPath = path.join(process.cwd(), 'npm-shrinkwrap.json');
+      if (err || !fs.existsSync(shWrpPath))
+        cb(err);
+      else {
+        const shrinkwrap = require(shWrpPath);
+        fixShwinkwrapDeps(shrinkwrap.dependencies, loc);
+        fs.writeFile(path.join('.cfpack', 'npm-shrinkwrap.json'),
+        JSON.stringify(shrinkwrap, undefined, 2), cb);
+      }
+    });
 };
 
 const executeZip = (directories, ignore, cb) => {
   // We're using the system zip command here, may be better to use a
   // Javascript zip library instead
   const files = '-type f -not -regex "\\./\\.cfpack/package\\.json" ' +
-  '-not -regex ".*/\\.git"  -not -regex ".*test\\.js"';
+    '-not -regex ".*/\\.git"  -not -regex ".*test\\.js"';
   const ex = cp.exec('(find . ' + directories + ' ' + files +
     ' | zip -q -x@' + ignore + ' -@ .cfpack/app.zip) && ' +
     '(zip -q -j .cfpack/app.zip .cfpack/package.json)', {
@@ -112,7 +136,7 @@ const rootDir = (dir) => {
   if (dir === '/') return dir;
   try {
     if (JSON.parse(fs.readFileSync(
-        path.resolve(dir, 'package.json')).toString()).name === 'cf-abacus')
+      path.resolve(dir, 'package.json')).toString()).name === 'cf-abacus')
       return dir;
     return rootDir(path.resolve(dir, '..'));
   }
@@ -121,47 +145,57 @@ const rootDir = (dir) => {
   }
 };
 
-// Package an app for deployment to Cloud Foundry
-const runCLI = () => {
-  // Parse command line options
-  commander
-  // Accept root directory of local dependencies as a parameter, default
-  // to the Abacus root directory
-    .option(
-      '-r, --root <dir>', 'root local dependencies directory',
-        process.env.ABACUS_ROOT || rootDir(process.cwd()))
-    .option(
-      '-a, --additional <dir>', 'additional directory that will be packed',
-    process.env.ADDITIONAL_PACK_DIR)
-    .parse(process.argv);
-
+const run = (root, additional, cb) => {
   // Create the directories we need
-  mkdirs(commander.root, commander.additional, (err) => {
+  mkdirs(root, additional, (err) => {
     if (err) {
       console.log('Couldn\'t setup cfpack layout -', err);
-      process.exit(1);
+      cb(err);
     }
 
     // Generate the repackaged package.json
-    repackage(commander.root, commander.additional, (err) => {
+    repackage(root, additional, (err) => {
       if (err) {
         console.log('Couldn\'t write package.json -', err);
-        process.exit(1);
+        cb(err);
       }
-
       // Produce the packaged app zip
       // zip(path.join(commander.root, '.gitignore'), (err) => {
-      zip(path.join(commander.root, '.gitignore'),
-        commander.additional ? path.join('.cfpack', additionalDir) : '',
+      zip(path.join(root, '.gitignore'),
+        additional ? path.join('.cfpack', additionalDir) : '',
         (err) => {
           if (err) {
             console.log('Couldn\'t produce .cfpack/app.zip -', err);
-            process.exit(1);
+            cb(err);
           }
+          cb();
         });
     });
   });
 };
 
+// Package an app for deployment to Cloud Foundry
+const runCLI = () => {
+  // Parse command line options
+  commander
+    // Accept root directory of local dependencies as a parameter, default
+    // to the Abacus root directory
+    .option(
+    '-r, --root <dir>', 'root local dependencies directory',
+    process.env.ABACUS_ROOT || rootDir(process.cwd()))
+    .option(
+    '-a, --additional <dir>', 'additional directory that will be packed',
+    process.env.ADDITIONAL_PACK_DIR)
+    .parse(process.argv);
+
+  run(commander.root, commander.additional, (err) => {
+    if (err)
+      process.exit(1);
+  });
+};
+
 // Export our CLI
 module.exports.runCLI = runCLI;
+// Export API access
+module.exports.run = run;
+
