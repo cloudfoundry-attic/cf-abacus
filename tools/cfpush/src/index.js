558238c9ee6fdeda3b99f623a102d233aafb0dc0
Hristo Iliev <hsiliev@gmail.com>
2017-07-11 15:09:04 +0300

    Enable graceful cleanup of temp dir

--- tools/cfpush/src/index.js
+++ tools/cfpush/src/index.js
@@ -6,12 +6,13 @@ const _ = require('underscore');
 const extend = _.extend;
 const noop = _.noop;
 
-const fs = require('fs');
 const path = require('path');
 const cp = require('child_process');
 
 const commander = require('commander');
+const fs = require('fs-extra');
 const tmp = require('tmp');
+tmp.setGracefulCleanup();
 const yaml = require('js-yaml');
 
 /* eslint no-process-exit: 1 */
@@ -62,12 +63,29 @@ const remanifest = (root, name, instances, conf, buildpack, prefix, cb) => {
     });
 };
 
+const prepareTmpDir = () => {
+  const tmpDir = tmp.dirSync({
+    prefix: 'cfpush_',
+    discardDescriptor: true,
+    unsafeCleanup: true
+  });
+
+  const cfTmpDir = path.join(tmpDir.name, '.cf');
+  const cfSettings = path.join(
+    process.env.CF_HOME || process.env.HOME, '.cf'
+  );
+  if (fs.existsSync(cfSettings))
+    fs.copySync(cfSettings, cfTmpDir);
+
+  return tmpDir;
+};
+
 // Push an app
 const push = (name, start, cb) => {
   const command = 'cf push ' +
     (start ? '' : '--no-start ') +
     '-f .cfpush/' + [name, 'manifest.yml'].join('-');
-  const tmpDir = tmp.dirSync({ prefix: 'cfpush_' });
+  const tmpDir = prepareTmpDir();
   const ex = cp.exec(command, {
     cwd: process.cwd(),
     env: extend(process.env, { CF_HOME: tmpDir.name })
