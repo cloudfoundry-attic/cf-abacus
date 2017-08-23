db15f681876fc3f72692b5d56f3629bcc79d61cf
Martin Aleksandrov <martin.d.aleksandrov@gmail.com>
2017-08-23 17:16:10 +0300

    Add unit tests for cfpush module

--- tools/cfpush/src/index.js
+++ tools/cfpush/src/index.js
@@ -8,58 +8,36 @@ const noop = _.noop;
 
 const path = require('path');
 const cp = require('child_process');
+const async = require('async');
 
 const commander = require('commander');
 const fs = require('fs-extra');
 const tmp = require('tmp');
+const remanifester = require('./lib/remanifester.js');
+
 tmp.setGracefulCleanup();
-const yaml = require('js-yaml');
 
-/* eslint no-process-exit: 1 */
-
-// Create the directories we need
 const mkdirs = (cb) => {
-  // Create .cfpush directory
   fs.mkdir('.cfpush', (err) => {
     if (err) noop();
     cb();
   });
 };
 
-// Adjust manifest.yml env variables
-const adjustManifest = (app, name, instances, conf, buildpack) => {
-  if (app) {
-    app.name = name;
-    app.host = name;
-    if (instances)
-      app.instances = parseInt(instances);
-    app.path = '../' + app.path;
-    if (conf) {
-      if (!app.env) app.env = {};
-      app.env.CONF = conf;
-    }
-    if (buildpack)
-      app.buildpack = buildpack;
-  }
-};
-
-// Write new manifest.yml
-const remanifest = (root, name, instances, conf, buildpack, prefix, cb) => {
+const remanifest = (name, instances, conf, buildpack, prefix, cb) => {
   fs.readFile(
     path.join(process.cwd(), 'manifest.yml'), (err, content) => {
       if (err) {
         cb(err);
         return;
       }
-      const yml = yaml.load(content);
-      const app = yml.applications[0];
-      const appName = prefix ? [prefix, name].join('') : name;
 
-      adjustManifest(app, appName, instances, conf, buildpack);
+      const props = { prefix, name, instances, buildpack, conf };
+      const adjustedManifest = remanifester.adjustManifest(content, props);
 
       fs.writeFile(
         path.join('.cfpush', [name, 'manifest.yml'].join('-')),
-        yaml.dump(yml), cb);
+        adjustedManifest, cb);
     });
 };
 
@@ -80,7 +58,6 @@ const prepareTmpDir = () => {
   return tmpDir;
 };
 
-// Push an app
 const push = (name, start, cb) => {
   const command = 'cf push ' +
     (start ? '' : '--no-start ') +
@@ -104,7 +81,6 @@ const push = (name, start, cb) => {
 
 // Package an app for deployment to Cloud Foundry
 const runCLI = () => {
-  // Parse command line options
   commander
   // Accept root directory of local dependencies as a parameter, default
   // to the Abacus root directory
@@ -121,31 +97,19 @@ const runCLI = () => {
       'start an app after pushing')
     .parse(process.argv);
 
-  // Create the directories we need
-  mkdirs((err) => {
-    if (err) {
-      console.log('Couldn\'t setup cfpack layout -', err);
-      process.exit(1);
+
+  async.series([
+    (callback) => mkdirs(callback),
+    (callback) => remanifest(commander.name, commander.instances,
+      commander.conf, commander.buildpack, commander.prefix, callback),
+    (callback) => push(commander.name, commander.start, callback)
+  ], (error, results) => {
+    if (error) {
+      console.log('Couldn\'t push app %s -', commander.name, error);
+      throw error;
     }
-
-    // Generate the updated manifest.yml
-    remanifest(commander.root, commander.name, commander.instances,
-      commander.conf, commander.buildpack, commander.prefix, (err) => {
-        if (err) {
-          console.log('Couldn\'t write manifest.yml -', err);
-          process.exit(1);
-        }
-
-        // Produce the packaged app zip
-        push(commander.name, commander.start, (err) => {
-          if (err) {
-            console.log('Couldn\'t push app %s -', commander.name, err);
-            process.exit(1);
-          }
-        });
-      });
   });
+
 };
 
-// Export our CLI
 module.exports.runCLI = runCLI;
---
+++ tools/cfpush/src/test/cfpush-test.js
@@ -0,0 +1,158 @@
+'use strict';
+
+const cfpush = require('..');
+const cp = require('child_process');
+const fs = require('fs-extra');
+const tmp = require('tmp');
+const commander = require('commander');
+const remanifester = require('../lib/remanifester.js');
+
+const preparedManifest = 'manifest';
+const adjustedManifest = 'adjusted-manifest';
+
+const prefix = 'prefix-';
+const adjustedName = 'adjusted-name';
+const adjustedInstances = 3;
+const adjustedConf = 'adjusted-conf';
+const adjustedBuildpack = 'adjusted-buildpack';
+
+const stubFileSystem = () => {
+  stub(fs, 'mkdir').callsFake((dirName, cb) => {
+    cb();
+  });
+
+  stub(fs, 'readFile').callsFake((filename, cb) => {
+    cb(undefined, preparedManifest);
+  });
+
+  stub(fs, 'writeFile', (filename, content, cb) => {
+    cb();
+  });
+
+  stub(fs, 'existsSync').returns(false);
+};
+
+const stubTmp = (tmpDir) => {
+  console.log('stub tmp');
+  stub(tmp, 'setGracefulCleanup');
+  stub(tmp, 'dirSync').returns(tmpDir);
+};
+
+const stubChildProcess = (error) => {
+  const stdEvent = {
+    on: (undefined, cb) => {}
+  };
+
+  const executable = {
+    stdout: stdEvent,
+    stderr: stdEvent,
+    on: stub().withArgs('close', sinon.match.any)
+      .callsFake((eventId, cb) => {
+        cb(error);
+      })
+  };
+
+  stub(cp, 'exec').returns(executable);
+};
+
+const stubCommander = () => {
+  stub(commander, 'option').returns(commander);
+  stub(commander, 'parse');
+  commander.name = adjustedName;
+  commander.instances = adjustedInstances;
+  commander.buildpack = adjustedBuildpack;
+  commander.conf = adjustedConf;
+  commander.prefix = prefix;
+};
+
+const stubRemanifester = () => {
+  stub(remanifester, 'adjustManifest')
+    .withArgs(preparedManifest)
+    .returns(adjustedManifest);
+};
+
+const tmpDir = {
+  name: 'test-tmp-dir',
+  removeCallback: stub()
+};
+
+stubFileSystem();
+stubTmp(tmpDir);
+stubCommander();
+stubRemanifester();
+
+describe('Test abacus cfpush', () => {
+  const manifestPath = `.cfpush/${adjustedName}-manifest.yml`;
+
+  context('when application is pushed', () => {
+
+
+    before(() => {
+      stubChildProcess();
+
+      process.env = {
+        CF_HOME: 'path'
+      };
+
+      cfpush.runCLI();
+    });
+
+    after(() => {
+      fs.writeFile.reset();
+      cp.exec.restore();
+    });
+
+    it('verify manifest is adjusted', () => {
+      assert.calledWithExactly(fs.writeFile,
+        manifestPath,
+        adjustedManifest,
+        sinon.match.any);
+    });
+
+    it('verify cf push executed', () => {
+      assert.calledWithExactly(cp.exec,
+      `cf push --no-start -f ${manifestPath}`,
+      sinon.match.has('env', { CF_HOME: tmpDir.name }));
+      assert.calledOnce(tmpDir.removeCallback);
+    });
+  });
+
+  context('when application push fails', () => {
+    before(() => {
+      stubChildProcess(new Error());
+
+      process.env.PUSH_RETRY = 2;
+
+      try {
+        cfpush.runCLI();
+      }
+      catch (e) {
+        // ...
+      }
+
+    });
+
+    after(() => {
+      fs.writeFile.reset();
+      cp.exec.restore();
+    });
+
+    it('verify manifest is adjusted once', () => {
+      assert.calledOnce(fs.writeFile);
+      assert.calledWithExactly(fs.writeFile,
+        manifestPath,
+        adjustedManifest,
+        sinon.match.any);
+    });
+
+    it('verify cf push execution was retried', () => {
+      assert.calledTwice(cp.exec);
+      assert.calledWithExactly(cp.exec,
+      `cf push --no-start -f ${manifestPath}`,
+      sinon.match.has('env', { CF_HOME: tmpDir.name }));
+      assert.calledOnce(tmpDir.removeCallback);
+    });
+
+  });
+
+});
