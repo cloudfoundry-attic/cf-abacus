cb28da47e8a7ea62e8dd24d949670160e5f194bb
Hristo Iliev <hsiliev@gmail.com>
2017-11-21 17:58:43 +0200

    Add localdev docker image (#810)

---
+++ etc/docker/abacus-localdev/start.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e -x
+
+pouch=5984
+collector=9080
+reporting=9088
+meter=9100
+accumulator=9200
+aggregator=9300
+cf_applications=9500
+cf_renewer=9501
+cf_services=9502
+provisioning_plugin=9880
+account_plugin=9881
+authserver_plugin=9882
+eureka_plugin=9990
+
+docker run -d -p $pouch:$pouch -p $collector:$collector -p $reporting:$reporting abacus-localdev
