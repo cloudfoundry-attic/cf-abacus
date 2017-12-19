9ef37927c9e860c40396b499c63da9bf210fd5eb
Hristo Iliev <hsiliev@gmail.com>
2017-12-19 12:22:03 +0200

    Disable coveralls for packages without tests

--- tools/cfpack/src/test/test.js
+++ tools/cfpack/src/test/test.js
@@ -1,27 +1,38 @@
 'use strict';
 
+const fs = require('fs');
+const os = require('os');
 const path = require('path');
 const rimraf = require('rimraf');
-const fs = require('fs');
 const cfpack = require('../index.js');
 
 describe('CF Pack', () => {
-  before((done) => {
-    process.chdir(__dirname);
-    if (fs.existsSync('.cfpack'))
-      rimraf('.cfpack', () => {
-        done();
-      });
-    else done();
+  const tempDir = fs.mkdtempSync(path.join(os.tmpdir(), 'foo-'));
+  const shrinkwrapJSON = path.join(tempDir, 'npm-shrinkwrap.json');
+
+  before(() => {
+    fs.copyFileSync(
+      path.join(__dirname, 'package.test.json'),
+      path.join(tempDir, 'package.json')
+    );
+    fs.copyFileSync(
+      path.join(__dirname, 'npm-shrinkwrap.test.json'),
+      shrinkwrapJSON
+    );
+    process.chdir(tempDir);
+  });
+
+  after(() => {
+    rimraf.sync(tempDir);
   });
 
   context('When executing CF pack command', () => {
     it('should repackage modules correctly', (done) => {
-      cfpack.run(path.resolve('../../../../'), undefined, (err) => {
+      cfpack.run(path.resolve(`${__dirname}/../../../../`), undefined, (err) => {
         expect(err).to.equal(undefined);
-        const npmShrinkwrapJson = require('./.cfpack/npm-shrinkwrap.json');
-        const npmShrinkwrapJsonOrig = require('./npm-shrinkwrap.json');
-        const packageJson = require('./.cfpack/package.json');
+        const npmShrinkwrapJson = require(path.join(tempDir, '.cfpack/npm-shrinkwrap.json'));
+        const npmShrinkwrapJsonOrig = require(shrinkwrapJSON);
+        const packageJson = require(path.join(tempDir, '.cfpack/package.json'));
         expect(packageJson.dependencies['abacus-batch']).to.equal(
           npmShrinkwrapJson.dependencies['abacus-batch'].resolved
         );
