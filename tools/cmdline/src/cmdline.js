b7036596e42bdf5f35f876983e733b47a2c06fae
Martin Aleksandrov <martin.aleksandrov@sap.com>
2018-01-05 18:05:38 +0200

    Adapt abacus-cmd line to latest cf cli version

--- tools/cmdline/src/cmdline.js
+++ tools/cmdline/src/cmdline.js
@@ -4,15 +4,13 @@ const fs = require('fs');
 const tmp = require('tmp');
 const execSync = require('child_process').execSync;
 
-const trim = (value) => value ? value.trim() : undefined;
-
 const execute = (command, showLog = true) => {
   if (showLog) console.log('> ' + command);
   const tmpFile = tmp.fileSync();
   const cmd = 'bash -c ' + `"set -o pipefail; ${command} 2>&1 | tee ${tmpFile.name}"`;
   try {
     execSync(cmd, { stdio: ['ignore', process.stdout, process.stderr] });
-    return trim(fs.readFileSync(tmpFile.name).toString());
+    return fs.readFileSync(tmpFile.name).toString();
   } catch (err) {
     err.stderr = fs.readFileSync(tmpFile.name);
     throw err;
