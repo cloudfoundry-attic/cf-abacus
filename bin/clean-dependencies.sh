a290ddb63effb734abfb5ace277044790d9d8cc2
Hristo Iliev <hsiliev@gmail.com>
2018-06-18 16:41:17 +0300

    Only delete Abacus dependencies

--- bin/clean-dependencies.sh
+++ bin/clean-dependencies.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-find . -name node_modules -type d -exec rm -rf {} \;
+SCRIPT_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$SCRIPT_DIR" ]]; then
+  SCRIPT_DIR="$PWD";
+fi
+
+find $SCRIPT_DIR/.. -name node_modules -type d -exec rm -rf {} \;
