9bedc954262bff5042eea44f3a2aa52a34dac9eb
Hristo Dobtchev <hristo.dobtchev@sap.com>
2018-08-14 10:01:02 +0300

    Improve healthchecker integration test (#1005)
    Signed-off-by: Denica Madzhirova <denica.madzhirova@sap.com>

--- test/extensions/src/test/healthchecker-test.js
+++ test/extensions/src/test/healthchecker-test.js
@@ -39,6 +39,7 @@ const authentication = {
   }
 };
 
+
 describe('abacus-extensions-healthchecker-test', () => {
   const prefix = process.env.ABACUS_PREFIX || '';
 
@@ -46,57 +47,48 @@ describe('abacus-extensions-healthchecker-test', () => {
     request.waitFor(`${healthchecker}/v1/healthcheck`, authentication, startTimeout, done)
   );
 
-  it('reports the health of client facing components', function(done) {
-    this.timeout(totalTimeout + 2000);
-
-    request.get(`${healthchecker}/v1/healthcheck`, authentication, (err, response) => {
+  const check = (alias, groups, done) => {
+    request.get(`${healthchecker}${alias}`, authentication, (err, response) => {
       debug('Healthchecker err %o, response %j', err, response);
 
       expect(err).to.be.undefined;
       expect(response.statusCode).to.equal(200);
-      expect(response.body).to.be.an('object').that.has.all.keys(
-        `${prefix}abacus-account-plugin`,
-        `${prefix}abacus-broker`,
-        `${prefix}abacus-provisioning-plugin`,
-        `${prefix}abacus-usage-collector`,
-        `${prefix}abacus-service-dashboard`,
-        `${prefix}abacus-usage-reporting`
-      );
+      expect(response.body).to.be.an('object').that.has.all.keys(groups);
 
       for(let appGroup of Object.keys(response.body)) {
         const appsResponses = Object.values(response.body[appGroup]);
-        appsResponses.every((responseCode) => expect(responseCode).to.equal(200));
+        appsResponses.every((responseCode) => expect(responseCode, appGroup + ': ' + responseCode).to.equal(200));
       }
-
       done();
     });
+  };
+
+
+  it('reports the health of client facing components', function(done) {
+    this.timeout(totalTimeout + 2000);
+    const testGroups = [
+      `${prefix}abacus-account-plugin`,
+      `${prefix}abacus-broker`,
+      `${prefix}abacus-provisioning-plugin`,
+      `${prefix}abacus-usage-collector`,
+      `${prefix}abacus-service-dashboard`,
+      `${prefix}abacus-usage-reporting`
+    ];
+    check('/v1/healthcheck', testGroups, done);
   });
 
   it('reports the health of internal components', function(done) {
     this.timeout(totalTimeout + 2000);
-
-    request.get(`${healthchecker}/v1/healthcheck/internal`, authentication, (err, response) => {
-      debug('Healthchecker err %o, response %j', err, response);
-
-      expect(err).to.be.undefined;
-      expect(response.statusCode).to.equal(200);
-      expect(response.body).to.be.an('object').that.has.all.keys(
-        `${prefix}abacus-usage-meter`,
-        `${prefix}abacus-cf-renewer`,
-        `${prefix}abacus-usage-accumulator`,
-        `${prefix}abacus-usage-aggregator`,
-        `${prefix}abacus-eureka-plugin`,
-        `${prefix}abacus-housekeeper`,
-        `${prefix}abacus-applications-bridge`,
-        `${prefix}abacus-services-bridge`
-      );
-
-      for(let appGroup of Object.keys(response.body)) {
-        const appsResponses = Object.values(response.body[appGroup]);
-        appsResponses.every((responseCode) => expect(responseCode).to.equal(200));
-      }
-
-      done();
-    });
+    const testGroups = [
+      `${prefix}abacus-usage-meter`,
+      `${prefix}abacus-cf-renewer`,
+      `${prefix}abacus-usage-accumulator`,
+      `${prefix}abacus-usage-aggregator`,
+      `${prefix}abacus-eureka-plugin`,
+      `${prefix}abacus-housekeeper`,
+      `${prefix}abacus-applications-bridge`,
+      `${prefix}abacus-services-bridge`
+    ];
+    check('/v1/healthcheck/internal', testGroups, done);
   });
 });
