87ba14e8d6a00d5a80b56ec0db7d74f479e250a0
Hristo Iliev <hsiliev@gmail.com>
2018-09-14 10:39:33 +0300

    Revert "Use latest mongodb 3.1 driver (#1039)"
    This reverts commit f634ad9488a512a320229c819aad8d6a21015d09.

--- test/metering/collector/src/test/test.js
+++ test/metering/collector/src/test/test.js
@@ -68,7 +68,7 @@ const customEnv = extend({}, process.env, { ABACUS_COLLECT_QUEUE:  consumerConfi
 const lifecycleManager = createLifecycleManager().useEnv(customEnv);
 
 describe('abacus-usage-collector-itest', () => {
-  before((done) => {
+  before(() => {
     const modules = [
       lifecycleManager.modules.eurekaPlugin,
       lifecycleManager.modules.provisioningPlugin,
@@ -77,10 +77,8 @@ describe('abacus-usage-collector-itest', () => {
     ];
 
     // drop all abacus collections except plans and plan-mappings
-    dbclient.drop(process.env.DB_URI, /^abacus-((?!plan).)*$/, (err) => {
-      if (err) return done(err);
+    dbclient.drop(process.env.DB_URI, /^abacus-((?!plan).)*$/, () => {
       lifecycleManager.startModules(modules);
-      return done();
     });
   });
 
@@ -163,6 +161,7 @@ describe('abacus-usage-collector-itest', () => {
     };
 
     const connectionManager = new ConnectionManager([rabbitUri]);
+
 
     debug('Creating consumer ...');
     const consumer = new Consumer(connectionManager, amqpMessageParser, consumerConfig);
