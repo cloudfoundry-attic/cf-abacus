408b24b6a48dfff52b63e8bd3e77d9e3d27ae535
denicaM <30797051+denicaM@users.noreply.github.com>
2018-09-21 11:42:19 +0300

    Shift timewindows for consumers (#1050)
    Signed-off-by: Hristo Iliev <hristo.iliev@sap.com>

--- test/aggregation/reporting/README.md
+++ test/aggregation/reporting/README.md
@@ -2,3 +2,26 @@ abacus-usage-reporting-itest
 ===
 
 Usage reporting service integration and performance tests.
+
+
+Test data generated with:
+```sh
+export SLACK=3D
+export TIME_WINDOWS_SIZES='{ "D" : 6 }'
+yarn start && yarn smoke
+
+# generate ratedUsage.json
+# 1. List all docs in abacus-aggregator-aggregated-usage
+# 2. Merge them in array in ratedUsage.json
+
+# generate report.json
+curl -H 'Content-Type: application/json' http://localhost:9088/v1/metering/organizations/us-south:a3d7fe4d-3cb1-4cc3-a831-ffe98e20cf27/aggregated/usage | jq . > report.json
+
+# generate offset_report.json
+yarn stop
+export ABACUS_TIME_OFFSET=345600000
+yarn start
+export DATE_IN_MS=$(node -e "console.log(new Date().valueOf() + 345600000)")
+curl -H 'Content-Type: application/json' "http://localhost:9088/v1/metering/organizations/us-south:a3d7fe4d-3cb1-4cc3-a831-ffe98e20cf27/aggregated/usage/$DATE_IN_MS" | jq . > offset_report.json
+```
+
--- test/aggregation/reporting/src/test/test.js
+++ test/aggregation/reporting/src/test/test.js
@@ -31,26 +31,52 @@ const totalTimeout = commander.totalTimeout || 60000;
 
 const aggregatordb = dataflow.db('abacus-aggregator-aggregated-usage');
 const dbBulk = yieldable.functioncb(aggregatordb.bulkDocs);
+
 const ratedUsage = require('./ratedUsage.json');
-const report = require('./report.json');
+
+const todaysReport = require('./report.json');
+const futureReport = require('./offset_report.json');
 
 const now = moment.now();
+
+const replaceTimePart = (string) => {
+  const parts = string.split('-');
+  parts[0] = dbclient.pad16(now);
+  return parts.join('-');
+};
+
+const changeTimestamps = (doc) => {
+  if (doc.spaces) {
+    if (doc.spaces.consumers)
+      doc.spaces[0].consumers[0].t = dbclient.pad16(now);
+    if (doc.spaces.t)
+      doc.spaces.t = replaceTimePart(doc.spaces.t);
+  }
+
+  // Space document
+  if (doc.consumers && doc.consumers.t)
+    doc.consumers.t = replaceTimePart(doc.consumers.t);
+
+  return doc;
+};
 
 const setupRatedUsageDocs = (templates) => {
   const ratedDocs = [];
   for(let template of templates) {
     const newId = dbclient.kturi(dbclient.k(template.id), now);
+    const newAccumulatedId = dbclient.tkuri(dbclient.k(template.accumulated_usage_id), now + '-0-0-1-0');
     const doc = extend({}, template, {
       id: newId,
       _id: newId,
+      accumulated_usage_id: newAccumulatedId,
       start: now,
       end: now,
       processed: now
     });
-    if (doc.spaces)
-      doc.spaces[0].consumers[0].t = dbclient.pad16(now);
-    ratedDocs.push(doc);
+
+    ratedDocs.push(changeTimestamps(doc));
   }
+
   return ratedDocs;
 };
 
@@ -73,56 +99,87 @@ const uploadRatedUsage = (done) => {
   });
 };
 
+const offset = () => process.env.ABACUS_TIME_OFFSET ? parseInt(process.env.ABACUS_TIME_OFFSET) : 0;
+
 // Get usage report, throttled to default concurrent requests
 const getReport = (orgId, cb) => {
-  debug(`Getting org report for ${orgId} at ${now}`);
+  const reportTime = now + offset() + 1;
+  debug(`Requesting org report for ${orgId} @ ${moment.utc(reportTime)}`);
   request.get(
     'http://localhost::port/v1/metering/organizations/:organization_id/aggregated/usage/:time',
     {
       port: 9088,
       organization_id: orgId,
-      time: now + 1
+      time: reportTime
     },
     (err, val) => {
       expect(err).to.equal(undefined);
-      debug(`Got report for ${orgId} at ${now}`);
+      debug(`Got report for ${orgId} @ ${moment.utc(val.body.processed)}`);
       cb(val.body);
     }
   );
 };
 
-const verifyReports = (cb) => {
+const verifyReports = (expectedReport, cb) => {
   const orgId = ratedUsage[0].organization_id;
-  const expectedReport = setupReport(report);
   getReport(orgId, (report) => {
     expect(report, `Report for ${orgId} does not match`).to.deep.equal(expectedReport);
     cb();
   });
 };
 
-describe('abacus-usage-reporting-itest', () => {
-  before(() => {
-    const modules = [lifecycleManager.modules.accountPlugin, lifecycleManager.modules.reporting];
+const checkReport = (expectedReport, done) => {
+  // Wait for usage reporting service to start
+  request.waitFor('http://localhost::p/batch', { p: 9088 }, startTimeout, (err) => {
+    // Failed to ping usage reporting service before timing out
+    if (err) throw err;
 
-    dbclient.drop(process.env.DB_URI, /^abacus-/, () => {
-      lifecycleManager.startModules(modules);
-    });
+    // Upload organization usage and verify
+    uploadRatedUsage(() => verifyReports(expectedReport, done));
+  });
+};
+
+describe('abacus-usage-reporting-itest', () => {
+  beforeEach((done) => {
+    dbclient.drop(process.env.DB_URI, /^abacus-/, done);
   });
 
-  after(() => {
+  afterEach(() => {
     lifecycleManager.stopAllStarted();
   });
 
-  it('report rated usage submissions', function(done) {
-    this.timeout(totalTimeout + 2000);
+  context('with the current time', () => {
+    const expectedReport = setupReport(todaysReport);
 
-    // Wait for usage reporting service to start
-    request.waitFor('http://localhost::p/batch', { p: 9088 }, startTimeout, (err) => {
-      // Failed to ping usage reporting service before timing out
-      if (err) throw err;
+    beforeEach(() => {
+      lifecycleManager.startModules([
+        lifecycleManager.modules.accountPlugin,
+        lifecycleManager.modules.reporting
+      ]);
+    });
 
-      // Upload organization usage and verify
-      uploadRatedUsage(() => verifyReports(done));
+    it('reports rated usage submissions', function(done) {
+      this.timeout(totalTimeout + 2000);
+      checkReport(expectedReport, done);
     });
   });
+
+  context('4 days in the future', () => {
+    const expectedReport = setupReport(futureReport);
+
+    beforeEach(() => {
+      process.env.ABACUS_TIME_OFFSET = 4 * 24 * 60 * 60 * 1000;
+
+      lifecycleManager.useEnv(process.env).startModules([
+        lifecycleManager.modules.accountPlugin,
+        lifecycleManager.modules.reporting
+      ]);
+    });
+
+    it('shifts windows', function(done) {
+      this.timeout(totalTimeout + 2000);
+      checkReport(expectedReport, done);
+    });
+  });
+
 });
