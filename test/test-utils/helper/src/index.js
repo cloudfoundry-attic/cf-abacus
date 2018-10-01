5a9dc6db844a3cd7c241564ebe2eb522d2c053b3
Adriana Stefanova <adriana.stefanova@sap.com>
2018-10-01 11:07:15 +0300

    Remove unnecesaasy export from helper

--- test/test-utils/helper/src/index.js
+++ test/test-utils/helper/src/index.js
@@ -13,14 +13,6 @@ const checkCorrectSetup = (env) => {
     throw new Error('This test cannot run without correct set up. Please check if all environment variables are set.');
 };
 
-const getAbacusDirPath = () => {
-  const currentDirName = __dirname;
-  const abacusProjectName = 'cf-abacus';
-  const lastSymbolIndex = currentDirName.indexOf(abacusProjectName) + abacusProjectName.length;
-  return `${currentDirName.substring(0, lastSymbolIndex)}`;
-};
-
 module.exports = {
-  checkCorrectSetup,
-  getAbacusDirPath
+  checkCorrectSetup
 };
