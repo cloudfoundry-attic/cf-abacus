7efb9560d52639fef1ef9d7dbd531b25977d8139
Adriana Stefanova <adriana.stefanova@sap.com>
2018-10-01 11:28:21 +0300

    unset environment returns one unset variable

--- test/test-utils/helper/src/index.js
+++ test/test-utils/helper/src/index.js
@@ -1,16 +1,21 @@
 'use strict';
 
-const { values, every } = require('underscore');
+const { pairs, every } = require('underscore');
 
-const allValuesAreSet = (env) => {
-  return every(values(env), (value) => {
-    return typeof value !== 'undefined';
+const insufficientSetUp = (env) => {
+  let unsetProperty;
+  let allPropertiesAreSet = every(pairs(env), (pair) => {
+    unsetProperty = pair[0];
+    return typeof pair[1] !== 'undefined';
   });
+  return allPropertiesAreSet ? undefined : unsetProperty;
 };
 
 const checkCorrectSetup = (env) => {
-  if (!allValuesAreSet(env))
-    throw new Error('This test cannot run without correct set up. Please check if all environment variables are set.');
+  if (insufficientSetUp(env))
+    throw new Error(
+      'This test cannot run without correct set up. Please check if all environment variables are set. ' +
+      `Check ${insufficientSetUp(env)}.`);
 };
 
 module.exports = {
