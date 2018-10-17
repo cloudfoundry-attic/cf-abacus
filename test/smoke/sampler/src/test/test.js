f6c20e50bb015ef10728cd5cccb94c262689c9fd
i344281 <denica.madzhirova@sap.com>
2018-10-17 08:46:05 +0300

    [WIP] add http status codes, refactor

--- test/smoke/sampler/src/test/test.js
+++ test/smoke/sampler/src/test/test.js
@@ -6,6 +6,7 @@ const oauth = require('abacus-oauth');
 const util = require('util');
 const request = require('request');
 const { omit } = require('underscore');
+const httpStatus = require('http-status-codes');
 
 const doPost = util.promisify(request.post);
 const doGet = util.promisify(request.get);
@@ -20,116 +21,163 @@ const testEnv = {
   clientId: process.env.SAMPLER_CLIENT_ID,
   clientSecret: process.env.SAMPLER_CLIENT_SECRET,
   sysClientId: process.env.SYSTEM_CLIENT_ID,
-  sysClientSecret: process.env.SYSTEM_CLIENT_SECRET
+  sysClientSecret: process.env.SYSTEM_CLIENT_SECRET,
+  totalTimeout: process.env.SMOKE_TOTAL_TIMEOUT || 60000,
+  pollInterval: process.env.POLL_INTERVAL || 300
 };
 
+const orgId = '62332d73-d1ce-4da1-8455-64cc0f7e0b2e';
+const eventBuilder = () => {
+  const getEvent = (time) => ({
+    timestamp: time,
+    organization_id: orgId,
+    space_id: 'bcedeb4a-641e-4d80-9e35-435cbaf79d5c',
+    consumer_id: '92d9ef8b-fd71-46d7-b460-e64f44e18f18',
+    resource_id: 'sampler-postgresql',
+    plan_id: 'v9.4-large',
+    resource_instance_id: '2249be66-9f05-4525-a09e-955ae2ab53c1',
+    measured_usage: []
+  });
+
+  return {
+    startEvent: (time) => getEvent(time),
+    stopEvent: (time) => omit(getEvent(time), 'measured_usage')
+  };
+};
+
+const getReport = async(token, time) => {
+  const response = await doGet(`/v1/metering/organizations/${orgId}/aggregated/usage/${time}`, {
+    baseUrl: testEnv.reportingUrl,
+    headers: {
+      authorization: token()
+    },
+    rejectUnauthorized: !testEnv.skipSSL
+  });
+  expect(response.statusCode).to.be.equal(httpStatus.OK, 'Did not get report');
+  return response.body;
+};
+
+const postMappings = async(token) => {
+  const response = await doPost('/v1/mappings', {
+    baseUrl: testEnv.receiverUrl,
+    json: {
+      'resource_id': 'sampler-postgresql',
+      'plan_id': 'v9.4-large',
+      'metering_plan': 'standard-services-hours',
+      'rating_plan': 'standard-services-hours',
+      'pricing_plan': 'standard-services-hours'
+    },
+    headers: {
+      authorization: token()
+    },
+    rejectUnauthorized: !testEnv.skipSSL
+  });
+    // TODO: discuss this and possibly fix
+  expect(response.statusCode).to.be.oneOf([httpStatus.CREATED, httpStatus.CONFLICT]);
+};
+
+const postEvent = async(token, endpoint, event) => {
+  return await doPost(endpoint, {
+    baseUrl: testEnv.receiverUrl,
+    json: event,
+    headers: {
+      authorization: token()
+    },
+    rejectUnauthorized: !testEnv.skipSSL
+  });
+};
+
+const sendStart = async(token, event) => {
+  const response = await postEvent(token, '/v1/events/start' , event);
+  expect(response.statusCode).to.be.equal(httpStatus.CREATED, 'Start event not accepted by receiver!');
+};
+
+const sendStop = async(token, event) => {
+  const response = await postEvent(token, '/v1/events/stop' , event);
+  expect(response.statusCode).to.be.equal(httpStatus.CREATED, 'Stop event not accepted by receiver!');
+};
+
+const tryToStop = async(token, event) => {
+  const response = await postEvent(token, '/v1/events/stop' , event);
+  expect(response.statusCode).to.be.oneOf([httpStatus.CREATED, httpStatus.UNPROCESSABLE_ENTITY], 'Stop event not accepted by receiver!');
+};
+
+const getMonthlySummaryValue = (report) => {
+  const resourceIndex = 0;
+  const aggrUsageMetricIndex = 0;
+  const monthWindowIndex = 4;
+  const currentMonthIndex = 0;
+  return JSON.parse(report)
+    .resources[resourceIndex]
+    .aggregated_usage[aggrUsageMetricIndex]
+    .windows[monthWindowIndex][currentMonthIndex]
+    .quantity;
+};
+
+
 describe('Sampler smoke test', () => {
+
   let eventToken;
   let reportToken;
   let initialReport;
 
-  const orgId = '62332d73-d1ce-4da1-8455-64cc0f7e0b2e';
-  const eventBuilder = (time) => {
-    const event = {
-      timestamp: time,
-      organization_id: orgId,
-      space_id: 'bcedeb4a-641e-4d80-9e35-435cbaf79d5c',
-      consumer_id: '92d9ef8b-fd71-46d7-b460-e64f44e18f18',
-      resource_id: 'sampler-postgresql',
-      plan_id: 'v9.4-large',
-      resource_instance_id: '2249be66-9f05-4525-a09e-955ae2ab53c1',
-      measured_usage: []
-    };
-
-    return {
-      startEvent: event,
-      stopEvent: omit(event, 'measured_usage')
-    };
-  };
-
-  const getReport = async(token, time) => {
-    const response = await doGet(`/v1/metering/organizations/${orgId}/aggregated/usage/${time}`, {
-      baseUrl: testEnv.reportingUrl,
-      headers: {
-        authorization: token()
-      },
-      rejectUnauthorized: !testEnv.skipSSL
-    });
-    expect(response.statusCode).to.be.equal(200, 'Did not get report');
-    return response.body;
-  };
-
-  const postEvent = async(token, endpoint, event) => {
-    return await doPost(endpoint, {
-      baseUrl: testEnv.receiverUrl,
-      json: event,
-      headers: {
-        authorization: token()
-      },
-      rejectUnauthorized: !testEnv.skipSSL
-    });
-  };
-
-  const sendStart = async(token, event) => {
-    const response = await postEvent(token, '/v1/events/start' , event);
-    expect(response.statusCode).to.be.equal(201, 'Start event not accepted by receiver!');
-  };
-
-  const sendStop = async(token, event) => {
-    const response = await postEvent(token, '/v1/events/stop' , event);
-    expect(response.statusCode).to.be.equal(201, 'Stop event not accepted by receiver!');
-  };
+  setEventuallyPollingInterval(testEnv.pollInterval);
+  setEventuallyTimeout(testEnv.totalTimeout);
 
 
-  before(async() => {
+  beforeEach(async() => {
+
     console.log('\n%s: Starting tokens ...', moment.utc().toDate());
+    // TODO: if secured check
     reportToken = oauth.cache(testEnv.api, testEnv.sysClientId, testEnv.sysClientSecret, 'abacus.usage.read abacus.usage.write');
     await util.promisify(reportToken.start)();
 
     eventToken = oauth.cache(testEnv.api, testEnv.clientId, testEnv.clientSecret, 'abacus.sampler.usage.write');
     await util.promisify(eventToken.start)();
 
-    console.log('\n%s: Getting initial report ...', moment.utc().toDate());
-    initialReport = await getReport(reportToken, moment.now());
-    // console.log('initial report: %o', initialReport);
-
-    // console.log('\n%s: Stopping eventual previous events before test execution ...', moment.utc().toDate());
-    // await sendStop(eventToken, eventBuilder(moment.utc().subtract(26, 'hours').valueOf()).stopEvent);
-
     console.log('\nPosting services mappings ...');
-    const res = await doPost('/v1/mappings', {
-      baseUrl: testEnv.receiverUrl,
-      json: {
-        'resource_id': 'sampler-postgresql',
-        'plan_id': 'v9.4-large',
-        'metering_plan': 'standard-services-hours',
-        'rating_plan': 'standard-services-hours',
-        'pricing_plan': 'standard-services-hours'
-      },
-      headers: {
-        authorization: eventToken()
-      },
-      rejectUnauthorized: !testEnv.skipSSL
-    });
+    await postMappings(eventToken);
 
-    console.log('>>>>>>>>>>>>>>>>>res.status: %s, body: %o', res.statusCode, res.body);
-    expect(res.statusCode).to.be.oneOf([201, 409]);
+    console.log('\n%s: Stopping eventual previous events before test execution ...', moment.utc().toDate());
+    const stopTimestamp = moment.utc().subtract(26, 'hours').valueOf();
+    const stopEvent = eventBuilder().stopEvent(stopTimestamp);
+    await tryToStop(eventToken, stopEvent);
+
+    console.log('\n%s: Retrieving initial report ...', moment.utc().toDate());
+    await eventually(async () => {
+      initialReport = await getReport(reportToken, moment.now());
+    });
+    console.log('initial report: %o', getMonthlySummaryValue(initialReport));
+
+  });
+
+  afterEach(async() => {
+    const stopTimestamp = moment.utc().subtract(26, 'hours').valueOf();
+    const stopEvent = eventBuilder().stopEvent(stopTimestamp);
+    await tryToStop(eventToken, stopEvent);
   });
 
   it('should receive sampler usage and Abacus processes it successfuly', async () => {
+    // TODO:
+    // readme
+    // corner cases with dates
+    // token start
 
     console.log('\n%s: Posting start event ...', moment.utc().toDate());
-    await sendStart(eventToken, eventBuilder(moment.utc().subtract(26 + 24, 'hours').valueOf()).startEvent);
+    const startTimestamp = moment.utc().subtract(26 + 24, 'hours').valueOf();
+    const startEvent = eventBuilder().startEvent(startTimestamp);
+    await sendStart(eventToken, startEvent);
 
     console.log('\n%s: Posting stop event ...', moment.utc().toDate());
-    await sendStop(eventToken, eventBuilder(moment.utc().subtract(26, 'hours').valueOf()).stopEvent);
+    const stopTimestamp = moment.utc().subtract(26, 'hours').valueOf();
+    const stopEvent = eventBuilder().stopEvent(stopTimestamp);
+    await sendStop(eventToken, stopEvent);
 
-    // wait to be processed
+    await eventually(async () => {
+      const report = await getReport(reportToken, moment.now());
+      expect(getMonthlySummaryValue(report)).not.to.be.equal(getMonthlySummaryValue(initialReport));
+      console.log('after report: %o', getMonthlySummaryValue(report));
+    });
 
-    console.log('\n%s: Getting final report ...', moment.utc());
-    const report = await getReport(reportToken, moment.now());
-    console.log('Result report: %o', report);
-
-    // compare reports
   });
 });
