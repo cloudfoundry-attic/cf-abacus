36bc99a8e84999f7dba2d4ec23e1fd048581a18f
Denica Madzhirova <denica.madzhirova@sap.com>
2018-10-19 17:15:10 +0300

    Sampler smoke test use current time
    Signed-off-by: Vasil Petkov <v.petkov@sap.com>

---
+++ test/smoke/sampler/src/test/config.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const env = {
+  api: process.env.CF_API_URI,
+  receiverUrl: process.env.RECEIVER_URL || 'http://localhost:7070',
+  reportingUrl: process.env.REPORTING_URL || 'http://localhost:9088',
+  skipSSL: process.env.SKIP_SSL_VALIDATION || false,
+  secured: process.env.SECURED === 'true',
+  clientId: process.env.SAMPLER_CLIENT_ID,
+  clientSecret: process.env.SAMPLER_CLIENT_SECRET,
+  systemClientId: process.env.SYSTEM_CLIENT_ID,
+  systemClientSecret: process.env.SYSTEM_CLIENT_SECRET,
+  totalTimeout: process.env.SMOKE_TOTAL_TIMEOUT || 60000,
+  pollInterval: process.env.POLL_INTERVAL || 300
+};
+
+const spanConfig = {
+  organization_id: '62332d73-d1ce-4da1-8455-64cc0f7e0b47',
+  space_id: 'bcedeb4a-641e-4d80-9e35-435cbaf79d5c',
+  consumer_id: '92d9ef8b-fd71-46d7-b460-e64f44e18f18',
+  resource_id: 'sampler-postgresql',
+  plan_id: 'v9.4-large',
+  resource_instance_id: '2249be66-9f05-4525-a09e-955ae2ab53c1',
+  metering_plan: 'standard-services-hours',
+  rating_plan: 'standard-services-hours',
+  pricing_plan: 'standard-services-hours'
+};
+
+const samplerConfig = {
+  stopEndpoint: '/v1/events/stop',
+  startEndpoint: '/v1/events/start',
+  mappingEndpoint: '/v1/mappings'
+};
+
+module.exports = {
+  env,
+  spanConfig,
+  samplerConfig
+};
---
+++ test/smoke/sampler/src/test/helpers.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const util = require('util');
+const request = require('request');
+const { omit } = require('underscore');
+
+const oauth = require('abacus-oauth');
+
+const { env, spanConfig, samplerConfig } = require('./config');
+
+const doGet = util.promisify(request.get);
+const doPost = util.promisify(request.post);
+
+const systemToken = env.secured ?
+  oauth.cache(env.api, env.systemClientId, env.systemClientSecret, 'abacus.usage.read abacus.usage.write') :
+  undefined;
+const samplerToken = env.secured ?
+  oauth.cache(env.api, env.clientId, env.clientSecret, 'abacus.sampler.write') :
+  undefined;
+
+const authHeader = (token) => token ? { authorization: token() } : {};
+
+const startTokens = async() => {
+  const startSystemToken = util.promisify(systemToken.start);
+  const startSamplerToken = util.promisify(samplerToken.start);
+
+  await startSystemToken();
+  await startSamplerToken();
+};
+
+const createEventBuilder = () => {
+  const _getEvent = (time) => ({
+    timestamp: time,
+    organization_id: spanConfig.organization_id,
+    space_id: spanConfig.space_id,
+    consumer_id: spanConfig.consumer_id,
+    resource_id: spanConfig.resource_id,
+    plan_id: spanConfig.plan_id,
+    resource_instance_id: spanConfig.resource_instance_id,
+    measured_usage: []
+  });
+
+  return {
+    createStartEvent: (time) => _getEvent(time),
+    createStopEvent: (time) => omit(_getEvent(time), 'measured_usage')
+  };
+};
+
+const createSamplerClient = () => {
+  const _postToSampler = async(endpoint, payload) => await doPost(endpoint, {
+    baseUrl: env.receiverUrl,
+    json: payload,
+    headers: authHeader(samplerToken),
+    rejectUnauthorized: !env.skipSSL
+  });
+
+  return {
+    stopSampling: async(event) => {
+      return await _postToSampler(samplerConfig.stopEndpoint, event);
+    },
+    startSampling: async(event) => {
+      return await _postToSampler(samplerConfig.startEndpoint, event);
+    },
+    createMapping: async(json) => await _postToSampler(samplerConfig.mappingEndpoint, json)
+  };
+};
+
+const reportClient = {
+  getReport: async(orgId, time) => {
+    const res = await doGet(`/v1/metering/organizations/${orgId}/aggregated/usage/${time}`, {
+      baseUrl: env.reportingUrl,
+      headers: authHeader(systemToken),
+      rejectUnauthorized: !env.skipSSL
+    });
+    return res;
+  }
+};
+
+module.exports = {
+  startTokens,
+  reportClient,
+  createEventBuilder,
+  createSamplerClient
+};
