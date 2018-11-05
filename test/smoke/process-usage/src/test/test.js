2ddabcd49dfab27f16b79bdfb014a97250823c65
Adriana Stefanova <adriana.stefanova@sap.com>
2018-11-05 14:56:28 +0200

    Fix process usage smoke test

--- test/smoke/process-usage/src/test/test.js
+++ test/smoke/process-usage/src/test/test.js
@@ -74,8 +74,7 @@ describe('process usage smoke test', function() {
 
   it('submits usage for a sample resource and retrieves an aggregated usage report', async function() {
     const timeout = Math.max(testEnv.totalTimeout, 40000) + 2000;
-    const startTime = moment.utc().startOf('month').valueOf() - 123;
-    // const startTime = moment.now();
+    const startTime = moment.now();
     const processingDeadline = startTime + timeout;
     const bytesInGigabyte = 1073741824;
 
