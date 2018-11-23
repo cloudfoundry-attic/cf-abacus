2da87d83707d5d69ed1533c44125199c2365900b
vgpetkov <v.petkov@sap.com>
2018-11-23 16:42:35 +0200

    Origin auth (#1123)
    * fix(smoke) add auth origin
    
    Signed-off-by: Hristo Dobtchev <hristo.dobtchev@sap.com>
    
    * fix(smoke) add origin for authentication
    
    Signed-off-by: Hristo Dobtchev <hristo.dobtchev@sap.com>

--- test/test-utils/cf-util/src/index.js
+++ test/test-utils/cf-util/src/index.js
@@ -4,8 +4,8 @@ const cmdline = require('abacus-cmdline');
 const appUtil = require('./cf-app');
 const serviceUtil = require('./cf-service');
 
-module.exports = ({ api, user, password }) => {
-  const cfUtils = cmdline.cfutils(api, user, password);
+module.exports = ({ api, user, password, origin }) => {
+  const cfUtils = cmdline.cfutils(api, user, password, origin);
   return {
     App: appUtil(cfUtils),
     Service: serviceUtil(cfUtils)
